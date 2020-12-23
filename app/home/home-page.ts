54deea382472efa5bed5a534b1551153dbd6b928
Nic <nic@nicandnat.com>
2020-12-22 21:46:34 -0600

    Minor styling and getting the app skeleton together

--- app/home/home-page.ts
+++ app/home/home-page.ts
@@ -2,15 +2,30 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { EventData } from "tns-core-modules/data/observable";
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
+import { Frame } from "tns-core-modules/ui/frame";
 
 import { HomeViewModel } from "./home-view-model";
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
     page.bindingContext = new HomeViewModel();
+    page.getViewById('adventurePlay').set('isEnabled',false);
 }
 
 export function onDrawerButtonTap(args: EventData) {
     const sideDrawer = <RadSideDrawer>app.getRootView();
     sideDrawer.showDrawer();
+}
+
+export function playCasual(args: EventData) {
+    console.log(`Playing casual game: ${args}`);
+    console.log(`Will navigate away from ${Frame.topmost().currentPage}`);
+}
+
+export function playTimed(args: EventData) {
+    console.log(`Playing timed game: ${args}`);
+}
+
+export function playAdventure(args: EventData) {
+    //TODO
 }
