5bd998f47b9c5d9faf8334a409d6cd175483e259
Nic <nic@nicandnat.com>
2021-01-02 16:16:13 -0600

    Add a puzzle for each of 4 selectable difficulties

---
+++ app/difficulty/difficulty-page.ts
@@ -0,0 +1,54 @@
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import * as app from "tns-core-modules/application";
+import { EventData, fromObject } from "tns-core-modules/data/observable";
+import { NavigatedData, Page } from "tns-core-modules/ui/page";
+import { Frame } from "tns-core-modules/ui/frame";
+import { knownFolders, path, File, Folder } from "tns-core-modules/file-system";
+
+export function onNavigatingTo(args: NavigatedData) {
+    const page = <Page>args.object;
+    const c = args.context;
+    const type = c == undefined ? 'casual' : c.type;
+    page.bindingContext = fromObject( { 'type' : type } );
+    page.getViewById('easy').on('tap', playEasy);
+    page.getViewById('medium').on('tap', playMedium);
+    page.getViewById('hard').on('tap', playHard);
+    page.getViewById('expert').on('tap', playExpert);
+}
+
+export function onDrawerButtonTap(args: EventData) {
+    const sideDrawer = <RadSideDrawer>app.getRootView();
+    sideDrawer.showDrawer();
+}
+
+export function playEasy(args: EventData) {
+    play( 'easy' );
+}
+
+export function playMedium(args: EventData) {
+    play( 'medium' );
+}
+
+export function playHard(args: EventData) {
+    play( 'hard' );
+}
+
+export function playExpert(args: EventData) {
+    play( 'expert' );
+}
+
+function play(difficulty: string) {
+    const games = <Array<any>>require('../games.json');
+    console.log( `There are ${games.length} games` );
+    const options = games.filter( g => g.difficulty == difficulty );
+    console.log( `There are ${options.length} games matching difficulty ${difficulty}` );
+    const idx = Math.floor( Math.random() * options.length );
+    const selected = options[idx];
+    const game = selected.puzzle.join( ',' );
+    console.log( `Randomly selected ${game}` );
+    Frame.topmost().navigate({
+        moduleName: 'game/game-page',
+        context: { 'game' : game,
+            'type' : Frame.topmost().currentPage.bindingContext.type }
+    });
+}
